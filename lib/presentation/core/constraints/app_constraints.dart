b4d78f6dc9132837faa580fc780ea477d400d87d
Sahim Salem <sahim.salem@brainstation-23.com>
2025-05-19 11:15:53 +0600

    fix(folder): mantain proper clean layered architecture

--- lib/presentation/core/constraints/app_constraints.dart
+++ lib/presentation/core/constraints/app_constraints.dart
@@ -8,4 +8,59 @@ import 'package:flutter/material.dart';
 
     //Url
      static const String banner = 'https://i.ibb.co/R4CGpjzk/apple-iphone-12-spring21-purple-04202021-big-jpg-large-1.png';
+    
+    //data
+     static final productData = [
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Wireless Headphones',
+        'price': 120.00, // Fixed large price
+        'originalPrice': 200.00,
+        'rating': 2.5,
+      },
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Smartphone',
+        'price': 800.00,
+        'originalPrice': 1000.00,
+        'rating': 3.7,
+      },
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Build your own computer',
+        'price': 250.00,
+        'originalPrice': 300.00,
+        'rating': 1.3,
+      },
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Laptop',
+        'price': 100000000000000.00,
+        'originalPrice': 120000000000000000.00,
+        'rating': 3.6,
+      },
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Wireless Headphones',
+        'price': 120.00, // Fixed large price
+        'originalPrice': 200.00,
+        'rating': 2.5,
+      },
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Smartphone',
+        'price': 800.00,
+        'originalPrice': 1000.00,
+        'rating': 3.7,
+      },
+      {
+        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
+        'title': 'Build your own computer',
+        'price': 250.00,
+        'originalPrice': 300.00,
+        'rating': 1.3,
+      },
+    ];
+  
+  
   }
--- lib/presentation/features/home/widgets/product_list.dart
+++ lib/presentation/features/home/widgets/product_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nop_cart/presentation/core/constraints/app_constraints.dart';
 import 'package:nop_cart/presentation/core/widgets/product_card.dart';
 
 class ProductList extends StatelessWidget {
@@ -6,58 +7,7 @@ class ProductList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productData = [
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Wireless Headphones',
-        'price': 120.00, // Fixed large price
-        'originalPrice': 200.00,
-        'rating': 2.5,
-      },
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Smartphone',
-        'price': 800.00,
-        'originalPrice': 1000.00,
-        'rating': 3.7,
-      },
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Build your own computer',
-        'price': 250.00,
-        'originalPrice': 300.00,
-        'rating': 1.3,
-      },
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Laptop',
-        'price': 100000000000000.00,
-        'originalPrice': 120000000000000000.00,
-        'rating': 3.6,
-      },
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Wireless Headphones',
-        'price': 120.00, // Fixed large price
-        'originalPrice': 200.00,
-        'rating': 2.5,
-      },
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Smartphone',
-        'price': 800.00,
-        'originalPrice': 1000.00,
-        'rating': 3.7,
-      },
-      {
-        'imageUrl': 'https://images.unsplash.com/photo-1593642634367-d91a135587b5?auto=format&fit=crop&w=150&h=150&q=80',
-        'title': 'Build your own computer',
-        'price': 250.00,
-        'originalPrice': 300.00,
-        'rating': 1.3,
-      },
-    ];
-
+   
     final screenWidth = MediaQuery.of(context).size.width;
     final crossAxisCount = screenWidth < 400
         ? 2
@@ -68,18 +18,21 @@ class ProductList extends StatelessWidget {
                 : 5;
 
     return GridView.builder(
+      
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       padding: const EdgeInsets.all(8),
+      
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        
         maxCrossAxisExtent: 250, // Maximum width per card
         crossAxisSpacing: 8,
         mainAxisSpacing: 8,
         childAspectRatio: 0.75, // Adjusted for better content fit
       ),
-      itemCount: productData.length,
+      itemCount: AppConstraints.productData.length,
       itemBuilder: (context, index) {
-        final data = productData[index];
+        final data =  AppConstraints.productData[index];
         return ProductCard(
           imageUrl: data['imageUrl']! as String,
           title: data['title']! as String,
